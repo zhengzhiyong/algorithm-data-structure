624b24ca502fa97eaf370672c9403d274e523848
zhengzhiyong <zhengzhiyong@jumore.com>
2019-04-19 13:53:50 +0800

    test

--- my-algorithm-data-structure/src/main/java/com/algorithm/SuperNum.java
+++ my-algorithm-data-structure/src/main/java/com/algorithm/SuperNum.java
@@ -23,6 +23,9 @@ public class SuperNum {
 
 
     public int superDigit2(final int superNum,final int k){
+        if (superNum<Double.NEGATIVE_INFINITY){
+
+        }
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < k; i++) {
             stringBuilder.append(String.valueOf(superNum));
@@ -34,6 +37,17 @@ public class SuperNum {
     @Test
     public void test1(){
         //System.out.println(superDigit(9875));
+//        System.out.println(Math.pow(10,10000));
+//        System.out.println(Math.pow(10,9999));
+//        System.out.println(Math.pow(10,9999)==Math.pow(10,10000));
+//        System.out.println(Float.MAX_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Double.MAX_VALUE);
+//        System.out.println(Math.pow(10,10000)>Double.POSITIVE_INFINITY);
+//        System.out.println(Double.isInfinite(Math.pow(10,10000)));
+//        System.out.println(Double.MAX_VALUE>Float.MAX_VALUE);
+//        System.out.println(Float.MAX_VALUE>Integer.MAX_VALUE);
+//        System.out.println(123456<Double.POSITIVE_INFINITY);
         System.out.println(superDigit2(9875,2));
     }
 }
