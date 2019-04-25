f61106fd31fd30d57de78dfc81d39fc997931c2e
ZhiYong <zzy_ww@sina.cn>
2019-04-25 23:46:26 +0800

    2个具有相同元素个数的数组，交换数据，使他们的总和之差达到最小。

--- my-algorithm-data-structure/src/main/java/com/algorithm/TwoSubArryMinDiff.java
+++ my-algorithm-data-structure/src/main/java/com/algorithm/TwoSubArryMinDiff.java
@@ -6,73 +6,73 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
-/**
- * 本文说是《编程之美》2.18新思路，其实也是July的《微软等公司面试100题》上的32题的解法。
- * 两个序列大小均为n，序列元素的值为任一整数，无序；
- * 要求通过交换两个序列的元素，使序列a元素之和与序列b的元素之和的差最小（可能存在很多种组合，要求找出其中一种即可）。
- *
- * 《编程之美》2.18解法二中提到，从2n个数中找n个元素，有三种可能：大于Sum/2，小于Sum/2以及等于Sum/2。而大于Sum/2与小于等于Sum/2没区别，
- * 故可以只考虑小于等于Sum/2的情况，这一点我们仍然沿用这个思想。
- */
 public class TwoSubArryMinDiff {
-
-    public static void main(String[] args) {
-        //int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
-        //int[] array = new int[]{1,10,30,50};
-        //getTwoSubArrayMinDiff(array);
-    }
-
     /**
-     * 两个序列大小均为n，序列元素的值为任一整数，无序；
-     * 要求通过交换两个序列的元素，使序列a元素之和与序列b的元素之和的差最小。
+     * 华为面试题（8分钟写出代码） 有两个数组a,b，大小都为n,数组元素的值任意，无序；
+     * 要求：通过交换a,b中的元素，使数组a元素的和与数组b元素的和之间的差最小
      *
      * 思路：
-     *     1、对数组排序。定义好两个数组a，数组b
-     *     2、根据两个数组的总和和数组的长度，去判断往哪个数组中添加元素。
-     *
      *     https://blog.csdn.net/qq_15720911/article/details/77860012
+     *
+     *     1、分别计算a、b两个数组的元素之和suma和sumb，并求他们的差diff=suma-sumb(这里diff=-5);
+     * 	   2、对单个元素进行操作，a[0]-b[0]=1-5=-4,这里a[0]和b[0]的差值小于a数组和b数组整体的差，交换他们，此时，a={5,2},b={1,3},diff=3;
+     * 	       然后继续a[0]-b[1]=5-3=2，2<3=diff,交换a[0]和b[1],此时a={3,2},b={1,5}，差值为diff=-1.到此，一次循环结束。
+     * 	   3、按照步骤2的方法,把数组a的元素从头到尾和b数组中所有元素比较，直到最后一个数组元素。
      */
 
-    @Test
-    public void getTwoSubArrayMinDiff(){
-        int[] array = new int[]{1,2,4,8,20,100};
-        int[] array1 = new int[0];
-        int[] array2 = new int[0];
-        int c = 0;
-        int b = 0;
-        Arrays.sort(array);
+    //合并两个数组
+    public static int[] megerArrays(int[] array1,int[] array2){
+        int length1 = array1.length;
+        int length2 = array2.length;
+        //将数组扩容
+        array1 = Arrays.copyOf(array1,length1+length2);
+        //将数组array2复制到扩容后的数组array1中，从位置length1开始，往后顺延length2个长度
+        System.arraycopy(array2,0,array1,length1,length2);
+        //返回扩容后的数组
+        return array1;
+    }
 
-        for(int i = 0;i<array.length ; i++){
-            if(b >= c || array2.length < array1.length){
-                c = c+array[array.length -1-i];
-                array2 = Arrays.copyOf(array2,array2.length+1);
-                array2[array2.length-1] = array[array.length -1-i];
-            }else {
-                b = b+array[array.length -1-i];
-                array1 = Arrays.copyOf(array1,array1.length+1);
-                array1[array1.length-1] = array[array.length -1-i];
+    public static void minDiff (int[] array1,int[] array2){
+        //计算出两个数组的差值
+        int diff = Arrays.stream(array1).sum() - Arrays.stream(array2).sum();
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array1.length; j++) {
+                //计算出两个数组中单个元素的差值
+                int singleDiff = array1[i] - array2[j];
+                //单个元素的差值和数组的差值比l较
+                if ((singleDiff - diff)*singleDiff<0){
+                    int temp = array1[i];
+                    array1[i] = array2[j];
+                    array2[j] = temp;
+                    //计算数据交换后两个数组的差值
+                    diff -= 2 * singleDiff;
+                }
             }
         }
-
-        System.out.println(Arrays.stream(array1).sum());
-        System.out.println(Arrays.stream(array2).sum());
-        System.out.println(Arrays.stream(array).sum());
-
-        for (int i : array1){
-            System.out.print("BB["+i+"]="+i+",");
-        }
+        Arrays.sort(array1);
+        Arrays.sort(array2);
+        int sum1 = Arrays.stream(array1).sum();
+        int sum2 = Arrays.stream(array2).sum();
+        System.out.println("两个数组总和为："+(sum1+sum2)+",array1总和为："+sum1+",array2总和为："+sum2);
+        IntStream.range(0,array1.length).forEach(i ->{
+            System.out.println("array1["+i+"]:"+array1[i]+"，array2["+i+"]:"+array2[i]);
+        });
     }
 
-    @Test
-    public void test2(){
-       // int[] array = new int[]{1,10,30,50};
-        int[] array1 = new int[]{1,10};
-        int[] array2 = new int[]{30,50};
+    public static void getTwoSubArrayMinDiff(int[] array){
+        int[] array1 = Arrays.copyOfRange(array,0,array.length/2);
+        int[] array2 = Arrays.copyOfRange(array,array.length/2,array.length);
+        minDiff(array1,array2);
+    }
 
-        int diff = Arrays.stream(array1).sum() - Arrays.stream(array2).sum();
-        IntStream.range(0,array1.length).forEach(i -> System.out.println());
-
-
-
+    public static void main(String[] args) {
+        int[] array1 = new int[]{1,10,20,40};
+        int[] array2 = new int[]{80,200,1000,2000};
+//        System.out.println("============================");
+        minDiff(array2,array1);
+//        System.out.println("============================");
+//        int[] array = megerArrays(array1,array2);
+//        getTwoSubArrayMinDiff(array);
     }
 }
+
