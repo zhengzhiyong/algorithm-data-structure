793ad8f99d8f0c239d3c963c9d5e2ce0870bf954
ZhiYong <zzy_ww@sina.cn>
2019-04-26 00:21:31 +0800

    解决冲突问题

--- my-algorithm-data-structure/src/main/java/com/algorithm/TwoSubArryMinDiff.java
+++ my-algorithm-data-structure/src/main/java/com/algorithm/TwoSubArryMinDiff.java
@@ -10,7 +10,6 @@ public class TwoSubArryMinDiff {
     /**
      * 华为面试题（8分钟写出代码） 有两个数组a,b，大小都为n,数组元素的值任意，无序；
      * 要求：通过交换a,b中的元素，使数组a元素的和与数组b元素的和之间的差最小
-<<<<<<< HEAD
      * 今天在富通天下面试刚好遇到这个题目(尬尴。没回答出来)
      * 思路：
      *     https://blog.csdn.net/qq_15720911/article/details/77860012
@@ -21,16 +20,6 @@ public class TwoSubArryMinDiff {
      * 	   3、按照步骤2的方法,把数组a的元素从头到尾和b数组中所有元素比较，直到最后一个数组元素。
      *
      * 	   4.利用(singleDiff - diff)*singleDiff<0为条件去判断是否数据交换,交换后利用(diff -= 2 * singleDiff)公式做差值。
-=======
-     *
-     * 思路：
-     *     https://blog.csdn.net/qq_15720911/article/details/77860012
-     *
-     *     1、分别计算a、b两个数组的元素之和suma和sumb，并求他们的差diff=suma-sumb(这里diff=-5);
-     * 	   2、对单个元素进行操作，a[0]-b[0]=1-5=-4,这里a[0]和b[0]的差值小于a数组和b数组整体的差，交换他们，此时，a={5,2},b={1,3},diff=3;
-     * 	       然后继续a[0]-b[1]=5-3=2，2<3=diff,交换a[0]和b[1],此时a={3,2},b={1,5}，差值为diff=-1.到此，一次循环结束。
-     * 	   3、按照步骤2的方法,把数组a的元素从头到尾和b数组中所有元素比较，直到最后一个数组元素。
->>>>>>> f61106fd31fd30d57de78dfc81d39fc997931c2e
      */
 
     //合并两个数组
@@ -43,7 +32,6 @@ public class TwoSubArryMinDiff {
         System.arraycopy(array2,0,array1,length1,length2);
         //返回扩容后的数组
         return array1;
-<<<<<<< HEAD
     }
 
     public static void minDiff (int[] array1,int[] array2){
@@ -92,37 +80,6 @@ public class TwoSubArryMinDiff {
         });
     }
 
-=======
-    }
-
-    public static void minDiff (int[] array1,int[] array2){
-        //计算出两个数组的差值
-        int diff = Arrays.stream(array1).sum() - Arrays.stream(array2).sum();
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array1.length; j++) {
-                //计算出两个数组中单个元素的差值
-                int singleDiff = array1[i] - array2[j];
-                //单个元素的差值和数组的差值比l较
-                if ((singleDiff - diff)*singleDiff<0){
-                    int temp = array1[i];
-                    array1[i] = array2[j];
-                    array2[j] = temp;
-                    //计算数据交换后两个数组的差值
-                    diff -= 2 * singleDiff;
-                }
-            }
-        }
-        Arrays.sort(array1);
-        Arrays.sort(array2);
-        int sum1 = Arrays.stream(array1).sum();
-        int sum2 = Arrays.stream(array2).sum();
-        System.out.println("两个数组总和为："+(sum1+sum2)+",array1总和为："+sum1+",array2总和为："+sum2);
-        IntStream.range(0,array1.length).forEach(i ->{
-            System.out.println("array1["+i+"]:"+array1[i]+"，array2["+i+"]:"+array2[i]);
-        });
-    }
-
->>>>>>> f61106fd31fd30d57de78dfc81d39fc997931c2e
     public static void getTwoSubArrayMinDiff(int[] array){
         int[] array1 = Arrays.copyOfRange(array,0,array.length/2);
         int[] array2 = Arrays.copyOfRange(array,array.length/2,array.length);
