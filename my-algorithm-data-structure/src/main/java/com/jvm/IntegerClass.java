df430443330a550f094f026bf815a9bca2387b42
zhengzhiyong <zhengzhiyong@jumore.com>
2019-05-06 19:11:21 +0800

    自动拆箱装箱

--- my-algorithm-data-structure/src/main/java/com/jvm/IntegerClass.java
+++ my-algorithm-data-structure/src/main/java/com/jvm/IntegerClass.java
@@ -12,6 +12,13 @@ public class IntegerClass {
         Integer a1 = new Integer(100);
         Integer b1 = new Integer(100);
         Integer c1 = new Integer(0);
+        Integer integer1 = 127;
+        Integer integer2 = 127;
+        Integer integer3 = 128;
+        Integer integer4 = 128;
+        Integer integer5 = -1;
+        Integer integer6 = -1;
+
         System.out.println(a == b);
         System.out.println(a == a1);
         System.out.println(a1 == b1);
@@ -20,6 +27,9 @@ public class IntegerClass {
         System.out.println( b == c + a);
         System.out.println(b == c + b);
         System.out.println(b1 == c1 + a1);
+        System.out.println(integer1==integer2);
+        System.out.println(integer3==integer4);
+        System.out.println(integer5==integer6);
     }
 }
 
